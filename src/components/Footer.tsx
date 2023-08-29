c2854bb814a7361596216693c739a45f260f3a43
Chad Chapman <chad.chapman2010@gmail.com>
2023-08-29 18:36:44 -0400

    Made following updates to footer: * Added content to footer. * Added styling to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
 import "../App.css";
 
+/**
+ * Renders component containing footer.
+ * @returns HTMLDivElement containing the footer.
+ */
 function Footer() {
     return (
         <div className="footer">
-            <h1>footer</h1>
+            <h2>Created by: Chad Chapman</h2>
+            <p className="footer-text">
+                <a className="footer-link" href="https://www.linkedin.com/in/chadchapman2010/">LinkedIn</a>
+            </p>
+            <p className="footer-text">
+                <a className="footer-link" href="https://github.com/chapmancbVCU">GitHub</a>
+            </p>
         </div>
     );
 }
