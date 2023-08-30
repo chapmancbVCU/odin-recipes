b397a7ddf721e2f98267c7737ea92f3608e238f9
Chad Chapman <chad.chapman2010@gmail.com>
2023-08-29 20:13:58 -0400

    Created a way render list from array imported from Recipes.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,17 @@
 //import React, { useState, useEffect } from "react";
 import "../App.css";
+import { recipeArray } from "../Recipes";
 function Home() {
 
     return (
         <div className="home">
             <h1>Home</h1>
+
+            <ul>
+                {recipeArray.map((recipe, index) => (
+                    <li key={index}>{recipe.name}</li>
+                ))}
+            </ul>
         </div>
     );
 };
