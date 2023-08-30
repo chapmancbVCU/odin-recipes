281ec79d3060bb1b64a267e32ddccd48fa0e9eed
Chad Chapman <chad.chapman2010@gmail.com>
2023-08-30 18:55:04 -0400

    Added more test objects to array

--- src/Recipes.tsx
+++ src/Recipes.tsx
@@ -1,4 +1,22 @@
 export const recipeArray:any[] = [
-    { id: 1, name: "blah", type: "dinner"}, 
-    { id: 2, name: "foo", type: "desserts"}
+    { 
+        id: 1, 
+        name: "blah", 
+        type: "dinner"
+    }, 
+    { 
+        id: 2, 
+        name: "foo", 
+        type: "desserts"
+    },
+    {
+        id: 3,
+        name: "bar",
+        type: "dinner"
+    },
+    {
+        id: 4,
+        name: "haha",
+        type: "desserts"
+    }
 ]
