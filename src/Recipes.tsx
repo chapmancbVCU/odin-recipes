e24083e2f3dfec1cc19a94ecfdfc0b4aab350943
Chad Chapman <chad.chapman2010@gmail.com>
2023-08-30 20:40:01 -0400

    Added first recipe

--- src/Recipes.tsx
+++ src/Recipes.tsx
@@ -6,8 +6,29 @@ export const recipeArray:any[] = [
     }, 
     { 
         id: 2, 
-        name: "foo", 
-        type: "desserts"
+        name: "Peanut Butter Banana Oat Bites", 
+        type: "desserts",
+        description: "These satisfying high-fiber bites make a great snack or quick breakfast. You can freeze a couple bites in a snack-size plastic bag for a grab and go breakfast too!",
+        prepTime: "15 minutes",
+        servings: "24",
+        ingredients: [
+            { ingredientName: "eggs", quantity: "1" },
+            { ingredientName: "ripe banana (mashed)", quantity: "1" },
+            { ingredientName: "peanut butter (heated in microwave for 30 seconds)", quantity: "1/2 cup"},
+            { ingredientName: "vanilla extract", quantity: "1 tsp"},
+            { ingredientName: "Splenda Brown Sugar blend", quantity: "2 tsp"},
+            { ingredientName: "old-fashioned rolled oats (not quick cooking) (gluten-free if needed)", quantity: "2 cup"},
+            { ingredientName: "baking soda", quantity: " tsp"},
+            { ingredientName: "salt", quantity: "1/2 tsp"},
+            { ingredientName: "ground flax seed", quantity: "1/4 cup"}
+        ],
+        directions: [
+            "Preheat oven to 350 degrees. Line a baking sheet with parchment paper.",
+            "In a medium bowl whisk together peanut butter, banana, egg, vanilla and Splenda Brown Sugar blend.",
+            "In a small bowl mix together oats, baking soda and salt. Add milled flaxseed.",
+            "Add oat mixture to peanut butter mixture and mix well.",
+            "Scoop batter into 1 Tbsp. balls and place on baking sheet. Bake for 10-12 minutes. Cool on wire rack."
+        ]
     },
     {
         id: 3,
