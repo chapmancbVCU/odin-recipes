4f34da92aba8ded2f8a25885b1c9fc532e2284e1
Chad Chapman <chad.chapman2010@gmail.com>
2023-08-30 20:40:09 -0400

    Initial commit

---
+++ src/components/Breakfast.tsx
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * IMPORTS                                                                    *
+ *****************************************************************************/
+import "../App.css";
+
+
+/**
+ * Renders the Breakfast component.
+ * @returns HTMLDivElement containing Breakfast component.
+ */
+function Breakfast() {
+    return (
+        <div>
+            <h1>Breakfast</h1>
+        </div>
+    );
+};
+
+export default Breakfast;
