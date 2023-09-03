e432c3dc04bf8c3ed6ed6f8bc226e2b817422221
Chad Chapman <chad.chapman2010@gmail.com>
2023-09-02 22:52:25 -0400

    Added route for individual recipe

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 import { HashRouter, Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
 import Nav from './components/Nav';
-
+import Recipe from './components/Recipe';
 
 /**
  * Renders the App.
@@ -26,6 +26,7 @@ function App() {
                     <Route path="/breakfast" element={<Breakfast />} />
                     <Route path="/dinner" element={<Dinner />} />
                     <Route path="/desserts" element={<Desserts />} />
+                    <Route path="/recipe/:id" element={<Recipe />} />
                 </Routes>
                 <Footer />
             </div>
