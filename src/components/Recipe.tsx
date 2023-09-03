27a435aed69a3c2a476a5e85b17681fce62e3155
Chad Chapman <chad.chapman2010@gmail.com>
2023-09-02 22:40:48 -0400

    Intial commit

---
+++ src/components/Recipe.tsx
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * IMPORTS                                                                    *
+ *****************************************************************************/
+import "../App.css";
+
+
+/**
+ * Renders the component for individual recipes.
+ * @returns HTMLDivElement containing Recipe component.
+ */
+function Recipe() {
+    return (
+        <div>
+            <h1>Recipe</h1>
+        </div>
+    );
+};
+
+export default Recipe;
