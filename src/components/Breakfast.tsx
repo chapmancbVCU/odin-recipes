4941195b886627d2422714f6f7d1b42552eaa173
Chad Chapman <chad.chapman2010@gmail.com>
2023-09-07 19:58:46 -0400

    Added list of dishes available for breakfast

--- src/components/Breakfast.tsx
+++ src/components/Breakfast.tsx
@@ -2,6 +2,8 @@
  * IMPORTS                                                                    *
  *****************************************************************************/
 import "../App.css";
+import { Link } from "react-router-dom";
+import { recipeArray } from "../js/Recipes";
 
 
 /**
@@ -9,9 +11,31 @@ import "../App.css";
  * @returns HTMLDivElement containing Breakfast component.
  */
 function Breakfast() {
+
+    const previewArray:any[] = [];
+
+    for(let i = 0; i < recipeArray.length; i++) {
+        if(recipeArray[i].type === "breakfast") {
+            previewArray.push(recipeArray[i]);
+            console.log(recipeArray[i].type)
+        }
+    }
     return (
         <div>
             <h1>Breakfast</h1>
+            <ul className="recipe-preview">
+                {previewArray.map((recipe, index) => (
+                    <li key={index}>
+                        <Link to={"/recipe/" + recipe.id}>
+                            <h3 className="recipe-name">{recipe.name}</h3>
+                            <img
+                                className="preview-img" 
+                                src={'/src/images/' + recipe.image}
+                            />
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
