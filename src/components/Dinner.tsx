a8e543e3832ed21d337594ff7e3a4e71a76b5a25
Chad Chapman <chad.chapman2010@gmail.com>
2023-09-07 20:04:14 -0400

    Added content for the dinner page

--- src/components/Dinner.tsx
+++ src/components/Dinner.tsx
@@ -2,6 +2,8 @@
  * IMPORTS                                                                    *
  *****************************************************************************/
 import "../App.css";
+import { Link } from "react-router-dom";
+import { recipeArray } from "../js/Recipes";
 
 
 /**
@@ -9,9 +11,31 @@ import "../App.css";
  * @returns HTMLDivElement containing Dinner component.
  */
 function Dinner() {
+
+    const previewArray:any[] = [];
+
+    for(let i = 0; i < recipeArray.length; i++) {
+        if(recipeArray[i].type === "dinner") {
+            previewArray.push(recipeArray[i]);
+            console.log(recipeArray[i].type)
+        }
+    }
     return (
         <div>
             <h1>Dinner</h1>
+            <ul className="recipe-preview">
+                {previewArray.map((recipe, index) => (
+                    <li key={index}>
+                        <Link to={"/recipe/" + recipe.id}>
+                            <h3 className="recipe-name">{recipe.name}</h3>
+                            <img
+                                className="preview-img" 
+                                src={'/src/images/' + recipe.image}
+                            />
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
