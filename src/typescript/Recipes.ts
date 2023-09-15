d12c225aac0aa392b39bf02196cd9d3372e746b3
Chad Chapman <chad.chapman2010@gmail.com>
2023-09-15 19:04:34 -0400

    Rename directory

---
+++ src/typescript/Recipes.ts
@@ -0,0 +1,115 @@
+export const recipeArray:any[] = [
+    { 
+        id: 1, 
+        name: "Breakfast Egg and Ham Burrito", 
+        type: "breakfast",
+        description: "If you are tired of the same boring breakfast, try this satisfying and tasty burrito. You can experiment with different veggies in this recipe too.",
+        prepTime: "15 minutes",
+        servings: "4",
+        ingredients: [
+            { ingredientName: "eggs", quantity: "4" },
+            { ingredientName: "egg white", quantity: 4 },
+            { ingredientName: "Dash hot pepper sauce", quantity: "1" },
+            { ingredientName: "black pepper", quantity: "1/4 tsp" },
+            { ingredientName: "cheddar cheese (reduced-fat, shredded)", quantity: "2 tbsp" },
+            { ingredientName: "margarine (trans fat-free)", quantity: "2 tsp" },
+            { ingredientName: "deli ham (reduced-sodium, chopped, (about 3 ounces))", quantity: "4 slices" },
+            { ingredientName: "onion(s) (diced)", quantity: "1/4 cup" },
+            { ingredientName: "green pepper (diced)", quantity: "1/4 cup" },
+            { ingredientName: "corn tortillas (heated)", quantity: "4" },
+            { ingredientName: "salsa", quantity: "4 tsp" },
+        ],
+        directions: [
+            "In a medium bowl, whisk together the eggs, egg whites, hot pepper sauce, black pepper, and cheese.",
+            "Heat the margarine in a medium non-stick pan over medium heat. Add the ham and sauté for 2-3 minutes. Remove the ham from the pan.",
+            "Add the onions and green peppers to the hot pan, and cook for about 5 minutes. Add the ham back to pan.",
+            "Reduce the heat to low and add the eggs to pan. Gently stir the eggs with a spoon or spatula and continue lightly cooking over low heat until the eggs are cooked and set.",
+            "Evenly divide the egg mixture into 4 servings. Spoon each portion of the egg mixture into a tortilla and top each with 1 tsp. salsa. Fold the tortilla to close."
+        ],
+        image: "breakfast-burrito_127230797_101119_3864451747.jpeg"
+    }, 
+    { 
+        id: 2, 
+        name: "Peanut Butter Banana Oat Bites", 
+        type: "dessert",
+        description: "These satisfying high-fiber bites make a great snack or quick breakfast. You can freeze a couple bites in a snack-size plastic bag for a grab and go breakfast too!",
+        prepTime: "15 minutes",
+        servings: "24",
+        ingredients: [
+            { ingredientName: "eggs", quantity: "1" },
+            { ingredientName: "ripe banana (mashed)", quantity: "1" },
+            { ingredientName: "peanut butter (heated in microwave for 30 seconds)", quantity: "1/2 cup" },
+            { ingredientName: "vanilla extract", quantity: "1 tsp" },
+            { ingredientName: "Splenda Brown Sugar blend", quantity: "2 tsp" },
+            { ingredientName: "old-fashioned rolled oats (not quick cooking) (gluten-free if needed)", quantity: "2 cup" },
+            { ingredientName: "baking soda", quantity: "1 tsp" },
+            { ingredientName: "salt", quantity: "1/2 tsp" },
+            { ingredientName: "ground flax seed", quantity: "1/4 cup" }
+        ],
+        directions: [
+            "Preheat oven to 350 degrees. Line a baking sheet with parchment paper.",
+            "In a medium bowl whisk together peanut butter, banana, egg, vanilla and Splenda Brown Sugar blend.",
+            "In a small bowl mix together oats, baking soda and salt. Add milled flaxseed.",
+            "Add oat mixture to peanut butter mixture and mix well.",
+            "Scoop batter into 1 Tbsp. balls and place on baking sheet. Bake for 10-12 minutes. Cool on wire rack."
+        ],
+        image: "peanut-butter-banana-oat-bites_diabetes-cookbook_081618_1021x779_3885281428.jpeg"
+    },
+    {
+        id: 3,
+        name: "Beef Stroganoff",
+        type: "dinner",
+        description: "Classic beef stroganoff. Save yourself prep time with this recipe by buying presliced mushrooms.",
+        prepTime: "15 minutes",
+        servings: "5",
+        ingredients: [
+            { ingredientName: "Ronzoni Healthy Harvest Whole Grain egg noodles (uncooked)", quantity: "5 oz" },
+            { ingredientName: "olive oil", quantity: "2 tsp" },
+            { ingredientName: "beef tenderloin (sliced into 2-inch strips)", quantity: "1 lbs" },
+            { ingredientName: "white (button) mushrooms (sliced)", quantity: "1 1/2 cup" },
+            { ingredientName: "onion(s) (minced)", quantity: "1/2 cup" },
+            { ingredientName: "all-purpose flour", quantity: "1 tbsp" },
+            { ingredientName: "dry white wine", quantity: "1/2 cup" },
+            { ingredientName: "Dijon Mustard", quantity: "1 tsp" },
+            { ingredientName: "fat free, low sodium beef broth", quantity: "1 (14.5-oz) can" },
+            { ingredientName: "fat-free sour cream", quantity: "1/2 cup" },
+            { ingredientName: "salt (optional)", quantity: "1/4 tsp" },
+            { ingredientName: "black pepper", quantity: "1/4 tsp" }
+        ],
+        directions: [
+            "Cook noodles according to package directions, omitting salt.",
+            "Add oil to a large sauté pan over high heat. Add meat and sauté for about 3 minutes. Remove meat from pan. Add mushrooms and onion and sauté for 5 minutes or until beginning to brown.",
+            "Add flour and cook for 1 minute. Add wine to deglaze pan; cook for 2 minutes. Add Dijon mustard and beef broth; bring to a boil. Reduce heat and simmer for 5 minutes.",
+            "Add beef and any juices back to broth and simmer for 3 more minutes. Add sour cream, salt (optional), and pepper; simmer for 30 seconds.",
+            "Serve over whole-grain egg noodles."
+        ],
+        image: "RecId_79_BeefStroganoff_Thinkstock487537715_021618_2383889296.jpg"
+    },
+    {
+        id: 4,
+        name: "Asian Pork Chops",
+        type: "dinner",
+        description: "Regular soy sauce contains wheat and gluten, so if you need to follow a gluten-free diet, make sure you purchase gluten-free soy sauce for this recipe.",
+        prepTime: "20 minutes, 10 minutes cooking",
+        servings: "4",
+        ingredients: [
+            { ingredientName: "soy sauce (reduced-sodium)", quantity: "2 tablespoons plus 1 teaspoon" },
+            { ingredientName: "rice wine vinegar", quantity: "3 tbsp" },
+            { ingredientName: "water", quantity: "2 tbsp" },
+            { ingredientName: "olive oil (divided use)", quantity: "1 tbsp plus 2 tsp" },
+            { ingredientName: "crushed red pepper flakes", quantity: "1/4 tsp" },
+            { ingredientName: "garlic (minced)", quantity: "1 clove" },
+            { ingredientName: "broccoli florets", quantity: "2 cup" },
+            { ingredientName: "pork loin boneless chops (divided into 4 portions)", quantity: "1 lbs" },
+            { ingredientName: "cilantro (chopped)", quantity: "2 tbsp" }
+        ],
+        directions: [
+            "In a medium bowl, combine the soy sauce, vinegar, water, 1 tablespoon olive oil, crushed red pepper, and garlic. Add the pork chops and marinate them in refrigerator for 20 minutes or longer.",
+            "Steam the broccoli for 5 minutes; until tender.",
+            "Add the remaining 2 tsp olive oil to a large sauté pan over medium-high heat. Add the pork chops and sear for about 5 minutes per side or until done. (Reserve marinade for later).",
+            "Place the marinade in a small saucepan. Bring it to a boil; reduce the heat and simmer about 2 minutes. Place the pork chops on serving platter and pour the cooked marinade over them.",
+            "Top the pork chops with chopped cilantro and serve with the steamed broccoli."
+        ],
+        image: "Asian-Pork-Chops_PNC_MG_0043-Edit-(ZF-5661-35964-1-078)_4054547978.jpg"
+    }
+]
